fc5a09303ea43e3a6ca83acb76c3ef6046745fd7
Jefferson <ifrit68@hotmail.com>
2023-04-15 19:42:53 -0400

    finish password hasher/user test

--- src/modules/user/pass-hasher/pass-hasher.service.spec.ts
+++ src/modules/user/pass-hasher/pass-hasher.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PassHasherService } from './pass-hasher.service';
+import * as bcrypt from 'bcryptjs';
 
 describe('PassHasherService', () => {
   let service: PassHasherService;
@@ -12,7 +13,42 @@ describe('PassHasherService', () => {
     service = module.get<PassHasherService>(PassHasherService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('hashPassword', () => {
+    it('should return a hashed password', async () => {
+      const password = 'password';
+
+      jest.spyOn(bcrypt, 'hash').mockImplementation(() => Promise.resolve('hashedpassword'));
+
+      const hashedPassword = await service.hashPassword(password);
+
+      expect(hashedPassword).toEqual('hashedpassword');
+      expect(bcrypt.hash).toHaveBeenCalledWith(password, 10);
+    });
+  });
+
+  describe('comparePassword', () => {
+    it('should return true if passwords match', async () => {
+      const password = 'password';
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      jest.spyOn(bcrypt, 'compare').mockImplementation(() => Promise.resolve(true));
+
+      const isMatch = await service.comparePassword(password, hashedPassword);
+
+      expect(isMatch).toBe(true);
+      expect(bcrypt.compare).toHaveBeenCalledWith(password, hashedPassword);
+    });
+
+    it('should return false if passwords do not match', async () => {
+      const password = 'password';
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      jest.spyOn(bcrypt, 'compare').mockImplementation(() => Promise.resolve(false));
+
+      const isMatch = await service.comparePassword('wrongpassword', hashedPassword);
+
+      expect(isMatch).toBe(false);
+      expect(bcrypt.compare).toHaveBeenCalledWith('wrongpassword', hashedPassword);
+    });
   });
 });
